7f77573baa41509c43a0751cae0d7dad3a991d7d
Wonkyoo Nam <dspaudio@gmail.com>
2026-02-04 12:01:39 +0900

    set soft wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,6 @@ vim.g.lazyvim_php_lsp = "intelephense"
 
 -- 외부 파일 수정 자동 감지 및 리로드
 vim.opt.autoread = true
+vim.opt.wrap = true -- 화면 끝에서 soft wrap
+vim.opt.linebreak = true -- 단어 기준 줄바꿈
+vim.opt.breakindent = true -- 줄 바뀐 부분 들여쓰기 (선택 사항)
